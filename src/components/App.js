3237b3389a5696b01b7bef14656ae3146c8d4eff
Shaon Dey <shaond3y@gmail.com>
2020-04-12 20:24:36 +0600

    Add redirect for about page

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
 import NotFoundPage from "./NotFoundPage";
 import Header from "./common/Header";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" exact component={HomePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
+        <Redirect from="/about-page" to="about" />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
