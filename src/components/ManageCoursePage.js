beef436187760bc645c2fdca7c97850fa838d449
Shaon Dey <shaond3y@gmail.com>
2020-04-13 19:30:09 +0600

    Integrate with api and save form data to db

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,26 +1,36 @@
 import React, { useState } from "react";
 import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
 
-const ManageCoursePage = props => {
+const ManageCoursePage = (props) => {
   const [course, setCourse] = useState({
     id: null,
     slug: "",
     title: "",
     authorId: null,
-    category: ""
+    category: "",
   });
 
   function handleChange({ target }) {
     setCourse({
       ...course,
-      [target.name]: target.value
+      [target.name]: target.value,
     });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course);
   }
 
   return (
     <>
       <h2>Manage Course</h2>
-      <CourseForm course={course} onChange={handleChange} />
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
